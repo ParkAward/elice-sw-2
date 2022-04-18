9a375a0e794cf490a09246f0acbed3f9ec5f3bed
ParkAward <parkSangjun1363@gmail.com>
2022-04-18 10:45:02 +0900

    jquery test

---
+++ 0413/test.js
@@ -0,0 +1,19 @@
+var blockOne = document.getElementById("red");
+var blockTwo = document.getElementById("yellow");
+var blockThree = document.getElementById("green");
+
+blockOne.addEventListener('mouseenter',e=>{
+    blockOne.setAttribute('class','red');
+});
+
+
+
+blockTwo.addEventListener('mouseenter',e=>{
+    blockTwo.setAttribute('class','red');
+});
+
+
+
+blockThree.addEventListener('mouseenter',e=>{
+    blockThree.setAttribute('class','red');
+});
